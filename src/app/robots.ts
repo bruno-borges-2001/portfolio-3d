785d9c415ced2c66e8e42cb2ba66b45c4246c2ca
Bruno Borges <brunoborges2001@gmail.com>
2024-02-29 16:00:48 -0300

    feat: update robots and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,8 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/api/', '/studio/']
     },
+    sitemap: process.env.NEXT_PUBLIC_METABASE + '/sitemap.xml'
   }
 }
